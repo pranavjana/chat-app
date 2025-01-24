7dc8ab38ac6de47acc05efcecbf0be6f6b74bd42
pranavjana <104693003+pranavjana@users.noreply.github.com>
2025-01-24 21:02:55 +0800

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import authRoutes from './routes/auth.routes.js';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
